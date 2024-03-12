7f9d6cd243132d93be4e3d7b3bbabcc6bd8600a9
anas.simplon <anas.simplon@gmail.com>
2024-03-12 11:12:24 +0000

    decryptage

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define decal 1
+
+void cryptage(char p[],int decalage) {
+    int i = 0;
+    while(p[i]!='\0') {
+        p[i] += decalage;
+        if(p[i] > 255) {
+            p[i] -= 255;
+        }
+        i++;
+    }
+}
+
+void decryptage(char p[],int decalage) {
+    int i = 0;
+    while(p[i]!='\0') {
+        p[i] -= decalage;
+        if(p[i] > 255) {
+            p[i] -= 255;
+        }
+        i++;
+    }
+}
+
+int main() {
+    char password[20];
+
+printf("Enter mot de passe\n");
+    scanf("%s",password);
+
+printf("\n\ncode cryptee avec decalage de %d",decal);
+    cryptage(password,decal);
+    printf("\n%s",password);
+
+printf("\n\ncode dycrypter avec decalage de %d\n\n",decal);
+    decryptage(password,decal);
+    printf("\n%s",password);
+
+
+}
